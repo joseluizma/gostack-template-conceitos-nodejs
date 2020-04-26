f260b2f1cb0bbdf80c55e300ce50c326ec85b709
joseluizma <joseluizma@hotmail.com>
2020-04-26 18:01:20 -0300

    #master - finalizando Desafio 02: Conceitos do Node.js

--- src/__tests__/repositories.spec.js
+++ src/__tests__/repositories.spec.js
@@ -58,16 +58,16 @@ describe("Repositories", () => {
     const response = await request(app)
       .put(`/repositories/${repository.body.id}`)
       .send({
-        url: "https://github.com/Rocketseat/unform",
-        title: "Unform",
+        url: "https://github.com/Rocketseat/umbriel",
+        title: "Umbriel",
         techs: ["React", "ReactNative", "TypeScript", "ContextApi"]
       });
 
     expect(isUuid(response.body.id)).toBe(true);
 
     expect(response.body).toMatchObject({
-      url: "https://github.com/Rocketseat/unform",
-      title: "Unform",
+      url: "https://github.com/Rocketseat/umbriel",
+      title: "Umbriel",
       techs: ["React", "ReactNative", "TypeScript", "ContextApi"]
     });
   });
